cdf44b05e7c950747ec47db73dcda2993e1fa9be
Jamie Vos <Jamievos100@gmail.com>
2026-01-08 14:20:22 +0100

    feat: Update navigatie menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 let hamburgerKnop = document.querySelector('#hamburgerKnop');
-let hamburgerCloseKnop = document.querySelector('ul h1')
-let hamburgerMenu = document.querySelector('#hamburgerMenu')
+let hamburgerCloseKnop = document.querySelector('#hamburgerCloseKnop');
+let hamburgerMenu = document.querySelector('#hamburgerMenu');
 
 hamburgerKnop.addEventListener('click', function() {
     hamburgerMenu.classList.add("show");
